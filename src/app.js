9b780c13cfb18ffc96f4ab5181e45ba225e1c133
krywinn <martakrywult@gmail.com>
2022-03-26 20:47:47 +0200

    Added timestamp from API (Last updated)

--- src/app.js
+++ src/app.js
@@ -1,3 +1,26 @@
+function formatTime(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${[day]}, ${hours}:${minutes}`;
+}
+
 function showWeather(response) {
   console.log(response.data);
   console.log(response.data.main.temp);
@@ -6,10 +29,11 @@ function showWeather(response) {
   console.log(response.data.weather[0].description);
   tempValue.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
-  humidity.innerHTML = response.data.main.humidity;
-  wind.innerHTML = Math.round(response.data.wind.speed);
-  weatherDescription.innerHTML = response.data.weather[0].description;
-  feelsLike.innerHTML = Math.round(response.data.main.feels_like);
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  feelsLikeElement.innerHTML = Math.round(response.data.main.feels_like);
+  timeElement.innerHTML = formatTime(response.data.dt * 1000);
 }
 
 let apiKey = "0ad145bfcc1ef1bfc5678ea389f3498a";
